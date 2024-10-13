78c08629d1b4bf683e6fa23dc4bb94edac655d32
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2024-10-13 17:24:13 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -173,14 +173,14 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
+    title: "Frontend Engineer",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "iOS and Android App Developer",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
@@ -194,8 +194,8 @@ export const workExperience = [
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Data Analyst & Power BI Developer",
+    desc: "I am a skilled data analyst specializing in transforming complex data into actionable insights to drive informed decision-making.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
